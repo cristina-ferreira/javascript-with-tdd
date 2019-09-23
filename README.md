5775e0f9c9e2cc813b723105e4789d2051de5c6f
cristinaferreira <cristina.d.ferreira@sapo.pt>
2019-09-23 11:14:02 +0100

    start repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-with-tdd
+
+wcs-javascript-02 js training with tdd part 1
---
+++ exercises/yell.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*
+ * Create a `yell` function that takes a string
+ * and return the same string in upper case
+ *
+ */
+function yell(s) {
+    return s.toUpperCase();
+}
+
+//* Begin of tests
+const assert = require('assert')
+
+assert.fail('You must write your own tests')
+// End of tests */
